e33270ab784b8de1290b549fa9ab524015812d05
FRTNX <frtnx@protonmail.com>
2024-09-02 14:17:32 +0200

    Further UI improvements

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ const Item = ({ icon, text }) => {
   const dividerColor = DIVCOLOR;
 
   return (
-    <div style={{ width: '100%', borderBottom: `3px solid ${dividerColor}`, fontSize: 15, textAlign: 'left' }}>
+    <div style={{ width: '100%', borderBottom: `2px solid ${dividerColor}`, fontSize: 15, textAlign: 'left' }}>
       <div style={{ display: 'inline-block', fontSize: 15, }}>
         {icon}
       </div>
@@ -76,7 +76,7 @@ const SideBar = ({ open }) => {
           },
         }}
       >
-        <p style={{ fontSize: 15, color: DIVCOLOR, paddingTop: 5 }}></p>
+        <p style={{ fontSize: 15, color: DIVCOLOR, paddingTop: 3 }}></p>
         <MenuItem>
           <Item icon={<FaTruck />} text={'Menu'} />
         </MenuItem>
